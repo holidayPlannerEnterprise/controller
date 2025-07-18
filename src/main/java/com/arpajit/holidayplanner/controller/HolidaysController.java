24cfb832018dc5f603f62b24794c2162e0ad106d
arpa2001 <arpajitofficial@gmail.com>
2025-07-18 19:38:17 +0530

    configured and added forst kafka comm

--- src/main/java/com/arpajit/holidayplanner/controller/HolidaysController.java
+++ src/main/java/com/arpajit/holidayplanner/controller/HolidaysController.java
@@ -1,7 +1,15 @@
 package com.arpajit.holidayplanner.controller;
 
+import java.time.LocalDateTime;
+
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.arpajit.holidayplanner.dto.*;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -9,8 +17,24 @@ import jakarta.servlet.http.HttpServletRequest;
 public class HolidaysController {
     private static final Logger logger = LoggerFactory.getLogger(HolidaysController.class);
 
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    private ControllerProducer controllerProducer;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("/allHolidayDetails")
     public void getAllHolidayDetails(HttpServletRequest httpRequest) throws Exception {
         logger.info("Requested {}: {}", httpRequest.getMethod(), httpRequest.getRequestURL());
+        // Wrap in Kafka envelope
+        controllerProducer.setRequestType("GET_ALL_HOLIDAYS");
+        controllerProducer.setSourceService("controller-service");
+        controllerProducer.setTimestamp(LocalDateTime.now().toString());
+        // Send to Kafka
+        kafkaTemplate.send("holidayplanner-creator", controllerProducer);
+        logger.info("Sent Kafka envelope: {}", objectMapper.writeValueAsString(controllerProducer));
     }
 }
