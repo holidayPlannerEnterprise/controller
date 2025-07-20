c1c4ffd78b4cf23a5ca3c9ed448f069ca47e1d62
arpa2001 <arpajitofficial@gmail.com>
2025-07-20 20:40:34 +0530

    added message auditing

--- src/main/java/com/arpajit/holidayplanner/controller/HolidaysController.java
+++ src/main/java/com/arpajit/holidayplanner/controller/HolidaysController.java
@@ -11,6 +11,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.arpajit.holidayplanner.dto.*;
+import com.arpajit.holidayplanner.data.model.*;
+import com.arpajit.holidayplanner.data.respository.*;
 
 @RestController
 @RequestMapping("/holidayplanner")
@@ -23,6 +25,9 @@ public class HolidaysController {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Autowired
+    private MessageAuditRepository messageAuditRepository;
+
     @GetMapping("/allHolidayDetails")
     public void getAllHolidayDetails(HttpServletRequest httpRequest) throws Exception {
         logger.info("Requested {}: {}", httpRequest.getMethod(), httpRequest.getRequestURL());
@@ -31,9 +36,16 @@ public class HolidaysController {
                                                 "com.arpajit.holidayplanner.controller.getAllHolidayDetails",
                                                 LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")),
                                                 null);
+        MessageAudits messageAudits = new MessageAudits();
+        messageAudits.setMsgRequestType(message.getRequestType());
+        messageAudits.setMsgSourceService(message.getSourceService());
+        messageAudits.setMsgTimestamp(message.getTimestamp());
+        messageAudits.setMsgPayload(message.getPayload());
+        messageAudits.setMsgStatus("DROPPED");
         String payload = objectMapper.writeValueAsString(message);
         // Send to Kafka
         kafkaTemplate.send("holidayplanner-creator", payload);
         logger.info("Sent Kafka envelope: {}", payload);
+        messageAuditRepository.save(messageAudits);
     }
 }
