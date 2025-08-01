8a0ecedf8a3c9dda9ace02ba09676f1895636f42
arpa2001 <arpajitofficial@gmail.com>
2025-08-01 11:19:44 +0530

    added repying kafka template for sync response

--- src/main/java/com/arpajit/holidayplanner/controller/MainController.java
+++ src/main/java/com/arpajit/holidayplanner/controller/MainController.java
@@ -2,10 +2,16 @@ package com.arpajit.holidayplanner.controller;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,14 +26,17 @@ public class MainController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    // @Autowired
+    // private KafkaTemplate<String, String> kafkaTemplate;
+
     @Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
+    private ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate;
 
     @Autowired
     private ControllerDataServComm dataService;
 
     @GetMapping("/allHolidayDetails")
-    public void getAllHolidayDetails(HttpServletRequest httpRequest) throws Exception {
+    public ResponseEntity<String> getAllHolidayDetails(HttpServletRequest httpRequest) throws Exception {
         logger.info("Requested {}: {}", httpRequest.getMethod(), httpRequest.getRequestURL());
         // Prepare topic message
         ProduceMessage message = new ProduceMessage(UUID.randomUUID().toString(),
@@ -37,13 +46,16 @@ public class MainController {
                                                     null,
                                                     "IN_QUEUE",
                                                     null);
-        logger.info("Sending Kafka envelope: {}", objectMapper.writeValueAsString(message));
         String payload = objectMapper.writeValueAsString(message);
         // Send to Kafka
-        logger.info("Sending Kafka envelope: {}", payload);
         String dataServiceResponse = dataService.addAudit(payload);
         logger.info("Response from Data Sercive on adding audit: ", dataServiceResponse);
-        kafkaTemplate.send("holidayplanner-creator", payload);
+        ProducerRecord<String, String> record = new ProducerRecord<>("holidayplanner-creator", payload);
+        logger.info("Sending Kafka envelope: {}", payload);
+        // kafkaTemplate.send("holidayplanner-creator", payload);
+        RequestReplyFuture<String, String, String> responsePayload = replyingKafkaTemplate.sendAndReceive(record);
         logger.info("Sent Kafka envelope: {}", payload);
+        ConsumerRecord<String, String> response = responsePayload.get(10, TimeUnit.SECONDS);
+        return ResponseEntity.ok(response.value());
     }
 }
